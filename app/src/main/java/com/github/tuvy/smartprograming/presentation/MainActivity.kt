406432ace82accca6f7157fce69fc323aa5e62f6
Tomosugi Tasaka <tuvy.ano@gmail.com>
2018-05-07 18:26:07 +0900

    確認用のviewを追加

--- app/src/main/java/com/github/tuvy/smartprograming/presentation/MainActivity.kt
+++ app/src/main/java/com/github/tuvy/smartprograming/presentation/MainActivity.kt
@@ -3,6 +3,8 @@ package com.github.tuvy.smartprograming.presentation
 import android.os.Bundle
 import com.github.tuvy.smartprograming.databinding.ActivityMainBinding
 import com.github.tuvy.smartprograming.presentation.misc.BaseActivity
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 
 class MainActivity : BaseActivity() {
 
@@ -14,4 +16,9 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
+//
+//    fun setupRecyclerView() {
+//        binding.recyclerView.adapter = GroupAdapter<ViewHolder>()
+//
+//    }
 }
